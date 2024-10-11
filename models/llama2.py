a56c2b856238634b7d495e2f0cdbfa1d085585a7
Eungis <eungizoa@gmail.com>
2024-10-11 16:25:25 +0900

    [feat] llama-2 SelfAttention and FeedForward Implementation

--- models/llama2.py
+++ models/llama2.py
@@ -49,7 +49,6 @@ def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, t
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_complex
     
-
 def apply_rotary_embeddings(x:torch.Tensor, freqs_complex: torch.Tensor, device: str):
     # Separate the last dimension pairs of two values, representing the real and imaginary parts of the complex number
     # Two consecutive values will become a single complex number
@@ -71,12 +70,28 @@ def apply_rotary_embeddings(x:torch.Tensor, freqs_complex: torch.Tensor, device:
     x_out = x_out.reshape(*x.shape).type_as(x).to(device)
     return x_out
 
+def repeat_kv(x:torch.Tensor, n_rep:int):
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    else:
+        return (
+            # |B, Seq_Len, H_KV, 1, Head_Dim|
+            x[:, :, :, None, :]
+            # |B, Seq_Len, H_KV, N_Rep, Head_Dim|
+            # .repeat(1, 1, 1, n_rep, 1)
+            .expand(batch_size, seq_len, n_kv_heads, n_rep, head_dim)
+            # |B, Seq_Len, H_KV * N_Rep, Head_Dim|
+            .reshape(batch_size, seq_len, n_kv_heads * n_rep, head_dim)
+        )
+
+
 class RMSNorm(nn.Module):
     
     def __init__(self, dim: int, eps: float=1e-6):
         super(RMSNorm, self).__init__()
         self.eps = eps
-        # The gamma parameter in the paper
+        # The `gamma` parameter in the paper
         self.weight = nn.Parameter(torch.ones(size=dim))
         
     def forward(self, x:torch.Tensor):
@@ -85,9 +100,113 @@ class RMSNorm(nn.Module):
         return x * torch.rsqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
 
 
+class SelfAttention(nn.Module):
+    
+    def __init__(self, args: ModelArgs):
+        super(SelfAttention, self).__init__()
+        
+        # The number of heads for the Keys and Values
+        self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+        # The number of heads for the Queries
+        # Note. `n_heads_q` cannot be `n_q_heads`. Otherwise it can't load the model weights.
+        self.n_heads_q = args.n_heads
+        # Indicates how many times the Keys and Values should be repeated.
+        self.n_rep = self.n_heads_q // self.n_kv_heads
+        self.head_dim = args.dim // args.n_heads
+        
+        self.wq = nn.Linear(args.dim, self.n_heads_q * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(self.n_heads_q * self.head_dim, args.dim)
+        
+        self.cache_k = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+        self.cache_v = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+        
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        batch_size, seq_len = x.shape # |B, 1, Dim|
+        
+        # |B, 1, Dim| -> |B, 1, H_Q * Head_Dim|
+        xq = self.wq(x)
+        # |B, 1, Dim| -> |B, 1, H_KV * Head_Dim|
+        xk = self.wk(x)
+        # |B, 1, Dim| -> |B, 1, H_VV * Head_Dim|
+        xv = self.wv(x)
+        
+        # |B, 1, H_Q * Head_Dim| -> |B, 1, H_Q, Head_Dim|
+        xq = xq.view(batch_size, seq_len, self.n_heads_q, self.head_dim)
+        # |B, 1, H_KV * Head_Dim| -> |B, 1, H_KV, Head_Dim|
+        xk = xk.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+        # |B, 1, H_KV * Head_Dim| -> |B, 1, H_KV, Head_Dim|
+        xv = xv.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+        
+        # |B, 1, H_Q, Head_Dim| -> |B, 1, H_Q, Head_Dim|
+        xq = apply_rotary_embeddings(xq, freqs_complex, device=x.device)
+        # |B, 1, H_KV, Head_Dim| -> |B, 1, H_KV, Head_Dim|
+        xk = apply_rotary_embeddings(xk, freqs_complex, device=x.device)
+        
+        # Fill the entry in cache
+        self.cache_k[:batch_size, start_pos:start_pos+seq_len] = xk
+        self.cache_v[:batch_size, start_pos:start_pos+seq_len] = xv
+        
+        # |B, Seq_Len_KV, H_KV, Head_Dim|
+        keys = self.cache_k[:batch_size, start_pos:start_pos+seq_len]
+        values = self.cache_k[:batch_size, start_pos:start_pos+seq_len]
+        
+        # Since every group of Q shares the same K and V heads, just repeat (copy) the K and V heads for every Q in the same group.
+        # |B, Seq_Len_KV, H_KV, Head_Dim| -> |B, Seq_Len_KV, H_Q, Head_Dim|
+        keys = repeat_kv(keys, self.n_rep)
+        values = repeat_kv(values, self.n_rep)
+        
+        # |B, 1, H_Q, Head_Dim| -> # |B, H_Q, 1, Head_Dim|
+        xq = xq.transpose(1, 2)
+         # |B, Seq_Len_KV, H_Q, Head_Dim| -> |B, H_Q, Seq_Len_KV, Head_Dim|
+        keys = keys.transpose(1, 2)
+        # |B, Seq_Len_KV, H_Q, Head_Dim| -> |B, H_Q, Seq_Len_KV, Head_Dim|
+        values = values.transpose(1, 2)
+        
+        # |B, H_Q, 1, Head_Dim| @ |B, H_Q, Head_Dim, Seq_Len_KV| -> |B, H_Q, 1, Seq_Len_KV|
+        scores = torch.matmul(xq, keys.transpose(2, 3)) / math.sqrt(self.head_dim)
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+        
+        # |B, H_Q, 1, Seq_Len_KV| @ |B, H_Q, Seq_Len_KV, Head_Dim}| -> |B, H_Q, 1, Head_Dim|
+        output = torch.matmul(scores, values)
+        # |B, H_Q, 1, Head_Dim| -> |B, 1, H_Q, Head_Dim| -> |B, 1, Dim|
+        output = output.transpose(1, 2).contiguous().reshape(batch_size, seq_len, -1)
+        # |B, 1, Dim|
+        return self.wo(output)
+    
+    
+class FeedForward(nn.Module):
+    
+    def __init__(self, args: ModelArgs):
+        super(FeedForward, self).__init__()
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+        # Round the hidden_dim to the nearest `multiple_of` parameter
+        hidden_dim = args.multiple_of * ((hidden_dim + args.multiple_of - 1) // args.multiple_of)
+        
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+        
+    def forward(self, x: torch.Tensor):
+        # |B, Seq_Len, Dim| -> |B, Seq_Len, Hidden_Dim|
+        swish = F.silu(self.w1(x))
+        # |B, Seq_Len, Dim| -> |B, Seq_Len, Hidden_Dim|
+        x_V = self.w3(x)
+        # |B, Seq_Len, Hidden_Dim| * |B, Seq_Len, Hidden_Dim| -> |B, Seq_Len, Hidden_Dim|
+        x = swish * x_V
+        # |B, Seq_Len, Hidden_Dim| -> |B, Seq_Len, Dim|
+        x = self.w2(x)
+        return x
+
+
 class EncoderBlock(nn.Module):
     
     def __init__(self, args: ModelArgs):
+        super(EncoderBlock, self).__init__()
         self.n_heads = args.n_heads
         self.dim = args.dim
         self.head_dim = args.dim // self.n_heads
@@ -108,11 +227,7 @@ class EncoderBlock(nn.Module):
         # |B, Seq_Len, Dim|
         out = h + self.feed_forward.forward(self.ffn_norm(h))
         return out
-        
 
-
-
-    
 class Transformer(nn.Module):
     
     def __init__(self, args: ModelArgs):
